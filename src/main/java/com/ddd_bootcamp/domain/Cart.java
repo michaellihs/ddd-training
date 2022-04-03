be96c7f67847c1912efa7efe3bcaf1bba8703670
Sandeep Jagtap <sandeep.jagtap@gmail.com>
2022-04-03 19:57:31 +0530

    Implemented code problem 3. Add 2 quantity of “Apple Pencil” to a Cart.

---
+++ src/main/java/com/ddd_bootcamp/domain/Cart.java
@@ -0,0 +1,21 @@
+package com.ddd_bootcamp.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private List<Item> items = new ArrayList<>();
+
+    public void add(Item item) {
+        items.add(item);
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    @Override
+    public String toString() {
+        return items.toString();
+    }
+}
---
+++ src/main/java/com/ddd_bootcamp/domain/Item.java
@@ -0,0 +1,16 @@
+package com.ddd_bootcamp.domain;
+
+public class Item {
+    private Product product;
+    private int quantity;
+
+    public Item(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return product.toString() + ":" + quantity;
+    }
+}
